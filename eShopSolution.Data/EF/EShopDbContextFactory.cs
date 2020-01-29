04d13dc843b68d755a0d2af9db191bed1ed4e577
Bach Ngoc Toan <ngoctoan@live.com>
2020-01-29 21:26:33 +0700

    Migration database

---
+++ eShopSolution.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("eShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- eShopSolution.Data/Entities/Order.cs
+++ eShopSolution.Data/Entities/Order.cs
@@ -7,7 +7,7 @@ namespace eShopSolution.Data.Entities
 {
    public class Order
     {
-        public string Id { set; get; }
+        public int Id { set; get; }
         public DateTime OrderDate { set; get; }
         public Guid UserId { set; get; }
         public string ShipName { set; get; }
