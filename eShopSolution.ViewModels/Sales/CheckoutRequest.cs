8a7f150cf499a4f8901b49ddff15955c602da7f8
Bach Ngoc Toan <ngoctoan@live.com>
2020-11-17 08:31:45 +0700

    Checkout the order

---
+++ eShopSolution.ViewModels/Sales/CheckoutRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.ViewModels.Sales
+{
+    public class CheckoutRequest
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public List<OrderDetailVm> OrderDetails { set; get; } = new List<OrderDetailVm>();
+    }
+}
